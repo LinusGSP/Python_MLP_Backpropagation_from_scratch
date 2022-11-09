78cc0280e7d332a8a7b7ab24af5a6ecfec364f6f
Linus <schoenbaechler.linus@gmail.com>
2022-11-09 10:44:22 +0100

    Simplified main file

--- src/main.py
+++ src/main.py
@@ -1,18 +1,14 @@
-import numpy as np
-
 from Networks.Backpropagation import Backpropagation, get_circle_examples
 
 
 def main():
-    np.set_printoptions(suppress=True)
-
     network = Backpropagation(
-        shape=(2, 20, 20, 20, 20, 2),
-        activations=(None, 2, 2, 2, 2, 1),
+        shape=(2, 8, 8, 2),
+        activations=(None, 2, 2, 1)
     )
 
     label, target = get_circle_examples(500)
-    network.train(label, target, 100)
+    network.train(label, target, 150)
     network.visualize_network()
 
 
